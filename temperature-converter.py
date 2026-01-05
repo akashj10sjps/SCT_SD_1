9c9034a3eb68e641b00bfef34ff6bfe7f006c53b
akashj10sjps <akashj10sjps@gmail.com>
2026-01-05 10:45:14 +0530

    Added temperature converter

---
+++ temperature-converter.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def celsius_to_kelvin(c):
+    return c + 273.15
+
+temp = float(input("Enter temperature in Celsius: "))
+
+print("Fahrenheit:", celsius_to_fahrenheit(temp))
+print("Kelvin:", celsius_to_kelvin(temp))
